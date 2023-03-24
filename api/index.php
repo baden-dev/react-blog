1dff616a6fea3d1d6c329b3dd7fb5677d0eda34e
bahja <baden021@student.wethinkcode.co.za>
2023-03-24 23:26:36 +0200

    Routing the request to the appropriate CRUD operation based on the method

---
+++ api/index.php
@@ -0,0 +1,30 @@
+<?php
+require 'connect_to_db.php';
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+echo "request_method = $request_method\n";
+
+switch ($request_method) {
+    case 'GET':
+        if (!empty($_GET["id"])) {
+            include('crud/read_one.php');
+        } else {
+            include('crud/read_all.php');
+        }
+        break;
+    case 'POST':
+        include('crud/create.php');
+        break;
+    case 'PUT':
+        include('crud/update.php');
+        break;
+    case 'DELETE':
+        include('crud/delete.php');
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(array("message" => "Method Not Allowed"));
+        break;
+}
+
+?>
