b94ce16655517ab9a11672a036839d3c69564ff9
bahja <baden021@student.wethinkcode.co.za>
2023-03-25 14:52:47 +0200

    Added the content type json header

--- api/crud/read_all.php
+++ api/crud/read_all.php
@@ -5,6 +5,8 @@ $query = "SELECT * FROM posts";
 if ($make_query= mysqli_query($connection, $query)) {
     $table_data = mysqli_fetch_all($make_query, MYSQLI_ASSOC);
     $json = json_encode($table_data, JSON_PRETTY_PRINT);
+
+    header('Content-Type: application/json');
     echo $json;
 
 } else {
