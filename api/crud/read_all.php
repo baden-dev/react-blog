78ac404f9695fafb7f30bd37e35b3b3ebd45f2ba
bahja <baden021@student.wethinkcode.co.za>
2023-03-25 14:41:20 +0200

    Read operation to read all posts from database

---
+++ api/crud/read_all.php
@@ -0,0 +1,14 @@
+<?php
+
+$query = "SELECT * FROM posts";
+
+if ($make_query= mysqli_query($connection, $query)) {
+    $table_data = mysqli_fetch_all($make_query, MYSQLI_ASSOC);
+    $json = json_encode($table_data, JSON_PRETTY_PRINT);
+    echo $json;
+
+} else {
+    $response = array("message" => "Error retrieving table records: " . mysqli_error($conn));
+    echo json_encode($response);
+}
+?>
