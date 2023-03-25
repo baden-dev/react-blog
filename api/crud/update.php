f2cceeb7dd91db90967d1068f93e68ece41f4014
bahja <baden021@student.wethinkcode.co.za>
2023-03-25 19:36:36 +0200

    Update operation to edit selected blog post

---
+++ api/crud/update.php
@@ -0,0 +1,19 @@
+<?php
+
+$data = json_decode(file_get_contents("php://input"), true);
+$title = $data["title"];
+$content = $data["content"];
+$post_date = $data["post_date"];
+$post_time = $data["post_time"];
+
+$id = $_GET["post_id"];
+$query = "UPDATE posts SET title='$title', content='$content', post_date='$post_date', post_time='$post_time' WHERE post_id=$id";
+
+if (mysqli_query($connection, $query)) {
+    $response = array("message" => "Record updated successfully");
+} else {
+    $response = array("message" => "Error updating record: " . mysqli_error($conn));
+}
+
+echo json_encode($response);
+?>
