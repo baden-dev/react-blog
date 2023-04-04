281b084e4efa562f6047a57a26e775f46bb29dbc
bahja <baden021@student.wethinkcode.co.za>
2023-04-04 23:10:27 +0200

    Added route for landing page which displays all posts though the AllPosts component

--- front_end/src/components/App.jsx
+++ front_end/src/components/App.jsx
@@ -1,8 +1,9 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./NavBar";
+import AllPosts from "./AllPosts";
 import "../css/App.css";
-
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <div className="page-content">
           <Header />
           <Navbar />
+          <Routes>
+            <Route path="/" element={<AllPosts />} />
+          </Routes>
         </div>
         <Footer className="page-footer" />
       </div>
